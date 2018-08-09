2d42845488d1edf6ed114aaf1d850db76a06ac9a
Frederik Seidler <Frederik.Seidler@gmx.de>
2018-08-09 20:29:45 +0200

    remove git test

--- src/main/java/de/seidfred/chain/ChainApplication.java
+++ src/main/java/de/seidfred/chain/ChainApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ChainApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Test");
+
 		SpringApplication.run(ChainApplication.class, args);
 	}
 }
